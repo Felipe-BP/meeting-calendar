499452a1c742c4ef3cb8803c3697c376541dff96
Felipe-BP <gvv.fel@gmail.com>
2019-04-23 23:44:58 -0300

    Initial Commit

---
+++ app/src/main/java/br/edu/utfpr/alunos/felipe/meetingcalendar/Add_Meeting.java
@@ -0,0 +1,13 @@
+package br.edu.utfpr.alunos.felipe.meetingcalendar;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Add_Meeting extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add__meeting);
+    }
+}
